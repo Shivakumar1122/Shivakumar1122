88185ee2898aa5419eb6020c0863f28df104bc1a
Shivakumar1122 <93638388+Shivakumar1122@users.noreply.github.com>
2022-01-05 17:55:38 +0530

    Create ec2.tf

---
+++ ec2.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "us-east-2"
+}
+resource "aws_instance" "myec2" {
+  ami           = "ami-0fb653ca2d3203ac1"
+  instance_type = "t2.micro"
+  subnet_id   = "subnet-0e386bdba714cf494"
+  key_name = "shivakey"
+  security_groups = ["sg-04638412bef8421f5"]
+  tags = {
+    Name = "Ec2tf"
+  }
+}
